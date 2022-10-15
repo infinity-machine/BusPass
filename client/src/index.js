bb0b3afa4480ed12c370b8d62ce0707365dfed29
Connor Carciofini <ccarciofini@gmail.com>
2022-10-15 14:35:06 -0500

    kill service worker

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { register as registerServiceWorker } from './serviceWorkerRegistration';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -12,4 +12,3 @@ root.render(
   </React.StrictMode>
 );
 
-registerServiceWorker();
--- client/src/service-worker.js
+++ client/src/service-worker.js
@@ -70,3 +70,17 @@ self.addEventListener('message', (event) => {
 });
 
 // Any other custom service worker logic can go here.
+// const refreshCacheAndReload = () => {
+//   if (caches) {
+//     // Service worker cache should be cleared with caches.delete()
+//     caches.keys().then((names) => {
+//       for (const name of names) {
+//         caches.delete(name);
+//       }
+//     });
+//   }
+//   // delete browser cache and hard reload
+//   window.location.reload(true);
+// };
+
+// export default refreshCacheAndReload;
